6ccb741237c9b830e037e09084d50e873bdd435e
Alex Mulwa <lexmulwa@gmail.com>
2018-08-28 20:07:51 +0300

    ballerintent

--- app/src/main/java/com/cerberus/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/cerberus/swoosh/LeagueActivity.kt
@@ -1,12 +1,22 @@
 package com.cerberus.swoosh
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class LeagueActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+
+
+    }
+
+    fun Next(view: View)
+    {
+        val n =Intent(this,SkillActivity::class.java)
+        startActivity(n)
     }
 }
